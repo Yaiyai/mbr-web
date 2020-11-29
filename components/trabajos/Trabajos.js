87f048435bccfa63c6d7b44ea310a4109250ff47
Yai <myd.rio@hotmail.com>
2020-11-29 17:13:49 +0100

    trabajos section done

--- components/trabajos/Trabajos.js
+++ components/trabajos/Trabajos.js
@@ -9,17 +9,36 @@ export const Trabajos = ({ sections }) => {
 	const isMounted = useRef(true)
 	const [allSections] = useState(sections)
 	const [trabajos, setTrabajos] = useState()
-	useEffect(() => {
-		return () => {
-			isMounted.current = false
-		}
-	})
+	const [items, setItems] = useState()
 	useEffect(() => {
 		const sectionID = '5fb157fefb855731d4d459dc'
 		if (isMounted.current) {
 			setTrabajos(allSections.find((elm) => elm._id === sectionID))
 		}
-	}, [])
+	}, [trabajos])
+
+	useEffect(() => {
+		if (isMounted.current) {
+			if (trabajos) {
+				setItems(
+					trabajos.gallery.map((elm) => {
+						return {
+							src: elm,
+							thumbnail: getThumbnails(elm),
+							w: 1200,
+							h: 900,
+						}
+					})
+				)
+			}
+		}
+	}, [trabajos, items])
+
+	useEffect(() => {
+		return () => {
+			isMounted.current = false
+		}
+	}, [items])
 
 	const getThumbnails = (str) => {
 		let splitStr = str.split('upload/')
@@ -27,19 +46,8 @@ export const Trabajos = ({ sections }) => {
 		return `${splitStr[0]}${newStr}${splitStr[1]}`
 	}
 
-	if (trabajos) {
-		const items = trabajos.gallery.map((elm) => {
-			return {
-				src: elm,
-				thumbnail: getThumbnails(elm),
-				w: 1200,
-				h: 900,
-			}
-		})
-		// console.log(items)
-	}
 	const getThumbnailContent = (item) => {
-		return <img src={item.thumbnail} width={120} height={90} />
+		return <img src={item.thumbnail} />
 	}
 
 	return (
@@ -48,7 +56,7 @@ export const Trabajos = ({ sections }) => {
 				<section id='trabajos' className='trabajos'>
 					<div className='container'>
 						<h2>{trabajos.title}</h2>
-						{/* <PhotoSwipeGallery items={items} thumbnailContent={getThumbnailContent} /> */}
+						<PhotoSwipeGallery items={items} thumbnailContent={getThumbnailContent} />
 					</div>
 				</section>
 			)}
